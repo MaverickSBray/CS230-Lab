f663667e1b0e4e6192a1b5b3562072c5b9756e3c
MaverickSBray <143005488+MaverickSBray@users.noreply.github.com>
2024-03-03 17:09:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
     <p>WVU ID: 800385067</p>
     <p>Hi, I am Maverick</p>
     </div>
+  <div className="card-container">
+    <Card title="Card 01" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
+    <Card title="Card 02" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
+    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content. />
+  </div>
   );
 }
 
