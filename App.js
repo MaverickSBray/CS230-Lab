63a47ea91a39e5fa966183832242f34328cb7528
MaverickSBray <msb00021@mix.wvu.edu>
2024-03-03 18:29:23 -0500

    3/3/2024 4.0

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Card from './Card';
 import Navbar from './Navbar';
 function App() {
   return (
+      <>
     <div className="App">
          <h1>CS 230L</h1>
     <h2>Section - 01</h2>
@@ -13,8 +14,9 @@ function App() {
   <div className="card-container">
     <Card title="Card 01" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
     <Card title="Card 02" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
-    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content. "/>
+    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
   </div>
+  </>
   );
 }
 
