ea52d91681c1e894a8582a18859a3c0454c1a196
MaverickSBray <msb00021@mix.wvu.edu>
2024-03-03 18:34:13 -0500

    3/3/2024 5.0

--- App.js
+++ App.js
@@ -2,21 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Card';
 import Navbar from './Navbar';
+
 function App() {
   return (
       <>
-    <div className="App">
-         <h1>CS 230L</h1>
-    <h2>Section - 01</h2>
-    <p>WVU ID: 800385067</p>
-    <p>Hi, I am Maverick</p>
-    </div>
-  <div className="card-container">
-    <Card title="Card 01" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
-    <Card title="Card 02" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
-    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
-  </div>
-  </>
+        <div className="App">
+          <h1>CS 230L</h1>
+          <h2>Section - 01</h2>
+          <p>WVU ID: 800385067</p>
+          <p>Hi, I am Maverick</p>
+        </div>
+        <div className="card-container">
+          <Card title="Card 01" content="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          <Card title="Card 02" content="Some quick example text to build on the card title and make up the bulk of the card's content." />
+          <Card title="Card 03" content="Some quick example text to build on the card title and make up the bulk of the card's content." />
+        </div>
+      </>
   );
 }
 
