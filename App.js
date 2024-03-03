8c4e8fefe0714f0ffe307adb7377480013a47117
MaverickSBray <msb00021@mix.wvu.edu>
2024-03-03 18:12:39 -0500

    3/3/2024 2.0

--- App.js
+++ App.js
@@ -5,11 +5,7 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    <h1>CS 230L</h1>
-=======
          <h1>CS 230L</h1>
->>>>>>> 74c08c7464494458b79f43017ba9ce208d6f6d6e
     <h2>Section - 01</h2>
     <p>WVU ID: 800385067</p>
     <p>Hi, I am Maverick</p>
@@ -17,9 +13,8 @@ function App() {
   <div className="card-container">
     <Card title="Card 01" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
     <Card title="Card 02" content="Some quick example teext to build on the card title and make up the bulk of the card's content." />
-    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content. />
+    <Card title="Card 03" content="Some quick example teext to build on the card title and make up the bulk of the card's content. "/>
   </div>
-
   );
 }
 
